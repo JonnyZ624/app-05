374cb73e9c9c77a7881387c5de2c7bc4a7431292
JonnyZ624 <tapiajonathan624@gmail.com>
2024-08-31 09:54:51 -0500

    Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { ProductosComponent } from './pages/productos/productos.component';
 import { LoginComponent } from './pages/login/login.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { privadoGuard } from './guards/privado.guard';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'privado',component:PrivadoComponent, canActivate:[privadoGuard]},
     {path:'productos',component:ProductosComponent},
     {path:'login',component:LoginComponent},
+    {path:'registro',component:RegistroComponent},
 
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'**',component:Error404Component}
